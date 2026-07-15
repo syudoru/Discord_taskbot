a5abc64e2704cabbd7eb45e99e6a7dc8c2006abc
Sdoru <99168505+syudoru@users.noreply.github.com>
2026-07-15 16:44:00 +0900

    postテスト

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const PORT = process.env.PORT || 8080;
 
 const publicKey = process.env.DISCORD_PUBLIC_KEY;
 
-console.log(publicKey);
+console.log(`public key: ${publicKey}`);
 
 app.get("/", (req, res) => {
   res.send("Discord Task Bot is running.");
 });
+
+app.post("/interactions", (req, res) =>{
+  console.log("Interaction received");
+
+  res.status(200).send("OK");
+})
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
