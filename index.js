73e6252b612258db73d6903e729c52f7ad8dbba7
Sdoru <99168505+syudoru@users.noreply.github.com>
2026-07-15 16:17:19 +0900

    公開鍵変数取得テスト

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const publicKey = process.env.DISCORD_PUBLIC_KEY;
+
+console.log(publicKey);
+
 app.get("/", (req, res) => {
   res.send("Discord Task Bot is running.");
 });
