aebc9c1398a73c9377205db4a64ac3b01dac8194
Sdoru <99168505+syudoru@users.noreply.github.com>
2026-07-16 11:41:39 +0900

    微修正

--- index.js
+++ index.js
@@ -1,89 +1,3 @@
-/*
-
-//dotenvライブラリで.envファイル読み込み
-require("dotenv").config();
-
-//discord-interactionsライブラリのverifyKeyのみ読み込み
-const { InteractionType, InteractionResponseType, verifyKey } = require("discord-interactions");
-
-//expressライブラリ読み込み
-const express = require("express");
-const app = express();
-
-const PORT = process.env.PORT || 8080;
-
-
-//環境変数取得
-const PUBLIC_KEY = process.env.DISCORD_PUBLIC_KEY;
-
-//jsonの生データを読み出すよう設定
-app.use(
-  express.json({
-    verify: (req, res, buf) => {
-      req.rawBody = buf.toString("utf8");
-    },
-  })
-);
-
-app.get("/", (req, res) => {
-  res.send("Discord Task Bot is running.");
-});
-
-app.post("/interactions", async (req, res) => {
-  console.log("Interaction received");
-
-  //署名とタイムスタンプ読み込み
-  const signature = req.get("X-Signature-Ed25519");
-  const timestamp = req.get("X-Signature-Timestamp");
-
-  if (!req.rawBody || !signature || !timestamp || !PUBLIC_KEY) {
-    //データ不備
-    return res.status(401).send("Insufficient data");
-  }
-
-  //discordの署名検証
-  try {
-    const isValid = await verifyKey(
-      req.rawBody,
-      signature,
-      timestamp,
-      PUBLIC_KEY
-    );
-
-    if (!isValid) {
-      //署名不許可
-      return res.status(401).send("invalid request signature");
-    }
-
-  } catch (err) {
-    //署名検証エラー
-    console.error(err);
-    return res.sendStatus(401);
-  }
-
-  //署名検証成功
-  console.log("Signature verification succeeded.");
-  //console.log(req.body);
-  console.log(req.rawBody);
-
-  // PING
-  if (interaction.type === InteractionType.PING) {
-    //PONG
-    const responseBody = { type: InteractionResponseType.PONG }
-    console.log("PONG response body:", JSON.stringify(responseBody));
-    return res.json(responseBody);
-  }
-
-  console.log("send 200");
-  res.status(200).send("OK");
-})
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-})
-
-*/
-
 //dotenvライブラリで.envファイル読み込み
 require("dotenv").config();
 //expressライブラリ読み込み
@@ -115,7 +29,9 @@ app.post('/interactions', async (req, res) => {
   try {
     const isValidRequest = await verifyKey(body, signature, timestamp, PUBLIC_KEY);
     if (!isValidRequest) {
-      return res.status(401).send('Bad request signature');
+      //署名不許可
+      console.log("invalid request signature");
+      return res.status(401).send("invalid request signature");
     }
   } catch (err) {
     //署名検証エラー
@@ -133,7 +49,7 @@ app.post('/interactions', async (req, res) => {
     //PONG
     const responseBody = { type: InteractionResponseType.PONG }
     console.log("PONG response body:", JSON.stringify(responseBody));
-    return res.json(responseBody);
+    return res.send(responseBody);
   }
 
   // スラッシュコマンドへの応答
