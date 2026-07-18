9baee805f14ead2a67796a6479542f187505eb75
Sdoru <99168505+syudoru@users.noreply.github.com>
2026-07-18 11:02:48 +0900

    コマンド自動登録テスト

---
+++ commands/todo/remove.js
@@ -0,0 +1,23 @@
+const { SlashCommandSubcommandBuilder } = require("discord.js");
+const { InteractionResponseType } = require("discord-interactions");
+
+module.exports = {
+  data: new SlashCommandSubcommandBuilder()
+    .setName("add")
+    .setDescription("タスクを削除します")
+    .addIntegerOption(option =>
+      option
+        .setName("task")
+        .setDescription("削除するタスクの番号")
+        .setRequired(true)
+    ),
+  execute(interaction) {
+
+    //タスク追加処理
+
+    return {
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: { content: "タスクを削除する予定" }
+    }
+  }
+}
