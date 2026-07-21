a2d7a9dffd53324a7793184f39bd36e155473a40
Sdoru <99168505+syudoru@users.noreply.github.com>
2026-07-21 12:08:53 +0900

    微修正

--- loaders/commandLoader.js
+++ loaders/commandLoader.js
@@ -2,14 +2,23 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath, pathToFileURL } from "url";
 
+console.log("4.3.1: module imported");
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
+console.log("4.3.2: get dirname");
 
 const commands = new Map();
 //コマンドファイルパス取得
 const commandsPath = path.join(__dirname, "../commands");
+
+console.log("4.3.3: get command path");
 //コマンドファイル・フォルダ取得
 const entries = fs.readdirSync(commandsPath, { withFileTypes: true });
+
+console.log("4.3.4: get command files");
 
 for (const entry of entries) {
   let filePath;
@@ -31,8 +40,10 @@ for (const entry of entries) {
     pathToFileURL(filePath).href
   );
 
+
   if ("data" in command && ("execute" in command || "subcommands" in command)) {
     commands.set(command.data.name, command)
+    console.log(`4.3.5: get command ${command.data.name}`);
   } else {
     console.warn(`[WARNING] ${filePath} には data または execute プロパティがありません。`);
   }
